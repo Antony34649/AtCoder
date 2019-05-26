8b5cce90fb7e00afac2c82adf206d5ea2af628de
Inoue Sora <skyflute4@gmail.com>
2019-05-26 14:27:03 +0900

    D問復習
    実行時間、メモリ制限がクリアできなかった

--- ABC127/D/D.cpp
+++ ABC127/D/D.cpp
@@ -1,58 +1,44 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <numeric>
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+bool Compare(pair<int, int> fArgu, pair<int, int> sArgu) {
+	return fArgu.second > sArgu.second;
+}
+
 int main() {
 	int n, m, i, j, x = 0;
-	long long ans=0;
 	cin >> n >> m;
-	vector<int> a(n), b(m) , c(m);
+	vector<int> a(n), d;
+	vector<long long> ans(n);
+	vector<pair<int, int>> p(m);
 	for (i = 0; i < n; i++) {
 		cin >> a[i];
 	}
 	for (i = 0; i < m; i++) {
-		cin >> b[i] >> c[i];
+		cin >> p[i].first >> p[i].second;
 	}
 
 	sort(a.begin(), a.end());
-	for (i = 1; i < m; i++) {
-		for (j = 0; j < m - i; j++) {
-			if (c[j] > c[j + 1]) {
-				int tmp = c[j];
-				c[j] = c[j + 1];
-				c[j + 1] = tmp;
-				
-				tmp = b[j];
-				b[j] = b[j + 1];
-				b[j + 1] = tmp;
-			}
+	sort(p.begin(), p.end(), Compare);
+
+	for (i = 0; i < m; i++) {
+		for (j = 0; j < p[i].first; j++) {
+			d.push_back(p[i].second);
 		}
 	}
 
-	for (j = m - 1; j >= 0; j--) {
-		for (i = n - 1; i >= 0;i--) {
-			if (a[i] < c[j]) {
-				x = i+1;
-				break;
-			}
-			else {
-				ans += a[i];
-			}
+	ans[0] = accumulate(a.begin(), a.end(), 0LL);
+	if (d.size() < n) {
+		for (i = 0; i < d.size(); i++) {
+			ans[i + 1] = ans[i] + (d[i] - a[i]);
 		}
-		
-		if (b[j] <= x) {
-			ans += c[j] * x;
-			cout << ans << endl;
-			return 0;
-		}
-		else {
-			ans += c[j] * b[j];
-		}
-
-		i = x-2-b[j];
 	}
+	else {
+		for (i = 0; i < n-1; i++) {
+			ans[i + 1] = ans[i] + (d[i] - a[i]);
+		}
+		ans.push_back(accumulate(d.begin(), d.begin() + (n - 1), 0LL));
+	}
+	cout << *max_element(ans.begin(), ans.end()) << endl;
 }
